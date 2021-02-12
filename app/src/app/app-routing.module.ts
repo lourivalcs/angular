2c79222b7674d1dcc2d2d8d79b96a9e79e092744
Lourival Costa da Silva <lourivalcs@gmail.com>
2021-02-12 15:36:02 -0300

    Added router

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent, TableComponent } from './components';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'table', component: TableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
